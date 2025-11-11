4aca882f7024d6318f274d7851086efd64e45ab8
YaeliP <porat4241@gmail.com>
2025-11-11 20:24:57 +0200

    from main

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const func1 = () => {
     console.log("Function111");
 }   
 const func2 = () => {
-    console.log("Function222");
+    console.log("from main");
 }   
 const func3 = () => {
     console.log("From main");
